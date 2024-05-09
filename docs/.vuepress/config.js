7b5bdb932df971f38ba655575880110dcff92044
zhaopeishen <zhaopeishen@yy.com>
2024-05-09 20:48:20 +0800

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    title: 'My VuePress Site',
+    description: 'A VuePress site with great SEO',
+    head: [
+      ['meta', { name: 'keywords', content: 'keyword1, keyword2' }]
+    ]
+  }
